df1d860ed68b2e26c473f1cac53ce45e1278de80
Marek Gibney <2785829+no-gravity@users.noreply.github.com>
2026-09-11 10:10:30 +0200

    Create git-complexity-diff.sh

---
+++ git-complexity-diff.sh
@@ -0,0 +1,35 @@
+working_diff() {
+    git diff "$@"
+    git ls-files --others --exclude-standard -z |
+        xargs -0 -r -I '{}' git diff --no-index "$@" \
+            -- /dev/null '{}'
+}
+
+print_number() {
+    if [ "$1" -gt 0 ]; then
+        printf '+'
+    fi
+    printf '%s\n' "$1"
+}
+
+echo -n 'Chars: '
+added=$(working_diff | grep '^+' | grep -v '^+++' |
+    cut -c2- | tr -d '\n' | wc -m)
+removed=$(working_diff | grep '^-' | grep -v '^---' |
+    cut -c2- | tr -d '\n' | wc -m)
+print_number $((added - removed))
+
+echo -n 'Lines: '
+added_lines=$(working_diff --numstat |
+    awk '{ added += $1 } END { print added+0 }')
+removed_lines=$(working_diff --numstat |
+    awk '{ removed += $2 } END { print removed+0 }')
+print_number $((added_lines - removed_lines))
+
+echo -n 'Long lines: '
+current_long=$(working_diff --unified=0 |
+    grep '^+' | grep -v '^+++' | cut -c2- |
+    awk 'length > 60 { count++ }
+        END { print count+0 }')
+last_long=$(git show HEAD --unified=0 | grep '^+' | grep -v '^+++' | cut -c2- | awk 'length > 60 { count++ } END { print count+0 }')
+print_number $((current_long - last_long))
